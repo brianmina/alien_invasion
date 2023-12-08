5eb43a851fd9fb661e5095aca33c04417761bc4d
brianmina <brian.mina17@gmail.com>
2023-12-08 15:56:01 -0500

    re-create the collision between ship and bottom check

--- alien_invasion.py
+++ alien_invasion.py
@@ -154,17 +154,27 @@ class AlienInvasion:
         # look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship,self.aliens):
             self._ship_hit()
+            # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
 
     def _ship_hit(self):
         """Respond to the ship being hit by alien."""
         # Decrement ships_left.
         self.stats.ships_left -= 1
-
         # Get rid of any remaining aliens and bullets.
         self.aliens.empty()
         self.ship.center_ship()
         # Pause.
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
 
 
